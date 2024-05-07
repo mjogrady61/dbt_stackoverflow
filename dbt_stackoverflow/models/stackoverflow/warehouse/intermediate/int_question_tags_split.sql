6e20a37dc4d19ed4d717ec4d2d8465c483f57ae5
mark-ogrady <mark.ogrady@farewill.com>
2024-05-07 21:17:21 +0100

    added dim_users table

---
+++ dbt_stackoverflow/models/stackoverflow/warehouse/intermediate/int_question_tags_split.sql
@@ -0,0 +1,27 @@
+
+
+
+WITH grouped_tags as (
+
+    SELECT 
+        GENERATE_UUID() AS surrogate_key_tags,
+        question_id,
+        question_tags
+    FROM {{ref('stg_stackoverflow__posts_questions')}}
+
+)
+
+
+, split_tags as (
+
+    SELECT
+        surrogate_key_tags,
+        question_id,
+        split_question_tags
+    from grouped_tags, UNNEST(SPLIT(question_tags,'|')) AS split_question_tags
+)
+
+-- This can act as a bridging table between questions and tags (topics) so we can build fact tables to analyse the tags and how active they are on SO
+
+select * from split_tags
+
