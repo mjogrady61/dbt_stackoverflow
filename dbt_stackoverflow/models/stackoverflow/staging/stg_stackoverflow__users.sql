371a66d194ff53f8c17b2460f1fb07d5f391eee1
mark-ogrady <mark.ogrady@farewill.com>
2024-05-07 19:39:37 +0100

    staged_user_table

---
+++ dbt_stackoverflow/models/stackoverflow/staging/stg_stackoverflow__users.sql
@@ -0,0 +1,26 @@
+
+with source as (
+
+    select * from {{source('stackoverflow','users')}}
+)
+
+, renamed as (
+
+    SELECT 
+       id                AS user_id,
+       display_name      AS user_display_name,
+       about_me          AS user_about_me,
+       age               AS user_age,
+       creation_date     AS user_creation_date,
+       last_access_date  AS user_last_access_date,
+       location          AS user_location,
+       reputation        AS user_reputation,
+       up_votes          AS user_up_votes,
+       down_votes        AS user_down_votes,
+       views             AS user_views,
+       profile_image_url AS user_profile_image_url,
+       website_url       AS user_website_url
+    FROM source
+)
+
+SELECT * FROM renamed
